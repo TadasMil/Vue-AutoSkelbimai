28725e21de23cfb8fab37adff34b8bdd0062d25e
Tadas Milieška <Tadas.milieska@bdo.global>
2021-03-11 11:38:31 +0200

    small changes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,7 @@ import { createApp } from "vue";
 import "./assets/tailwind.css";
 import App from "./App.vue";
 import router from "./routes/index.js";
-import store from "./store/index.js";
 
 createApp(App)
-  .use(store)
   .use(router)
   .mount("#app");
